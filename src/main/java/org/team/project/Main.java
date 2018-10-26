79ebfb677ce64a03de10710accefae5821fc87cb
Topiltzin <nokoayzack@gmail.com>
2018-10-26 17:01:23 -0500

    change setPreferredSize() to setSize()

--- src/main/java/org/team/project/Main.java
+++ src/main/java/org/team/project/Main.java
@@ -4,10 +4,14 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 
 public class Main {
+  private static final int PWIDTH = 500;
+  private static final int PHEIGHT = 400;
+  
   public static void main(String[] args) {
     JFrame frame = new JFrame("Reinos Olvidados");
 
     GamePanel gp = new GamePanel();
+    frame.setSize(new Dimension(PWIDTH, PHEIGHT));
     frame.getContentPane().add(gp, BorderLayout.CENTER);
     frame.setVisible(true);
     gp.setVisible(true);
