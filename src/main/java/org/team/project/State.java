7edc6f3cc7eb9f5907799fb21674fd43f81fc694
Celis <celistek92@gmail.com>
2018-11-13 16:30:38 -0600

    Borre State.java Running.java y Context.java

--- src/main/java/org/team/project/State.java
+++
@@ -1,8 +0,0 @@
-public interface State {
-    // Different states expected
-    // HasCard, NoCard, HasPin, NoCash
-    void isRunning();   
-    void gameOver();   
-    //void insertPin(int pinEntered);
-    //void requestCash(int cashToWithdraw);
-}
